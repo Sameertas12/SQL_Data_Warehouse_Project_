90730f0e5f87149eaf07c163056009de0fe50457
Sameer Mohammad <sameertas12@gmail.com>
2025-12-29 21:30:42 +0530

    Update load_data.sql

--- scripts/bronze/load_data.sql
+++ scripts/bronze/load_data.sql
@@ -1,9 +1,20 @@
+/*
+-- -----------------------------------------------------------------------------------------------------------------
+Loading data as it is into bronze layer tables
+
+Script Purpose:
+	This script loads the data from csv files to created tables in bronze layer.
+	Since null values are by default stored as 0 in sql tables, this script ensures 
+	that NULL and Blank values are stored as NULL itself in sql tables rather than 0(Same like in SQL Server).
+	Since this is Bronze layer and data should be loaded as it is, this step is prioritized
+-- -----------------------------------------------------------------------------------------------------------------
+*/
+
 
 
 -- -----------------------------------------------------------------------------------------------------------------
--- Loading data as it is into bronze layer tables
+-- CRM Folder: crm_cust_info Table
 -- -----------------------------------------------------------------------------------------------------------------
-
 LOAD DATA LOCAL INFILE 'C:/Users/samee/OneDrive/Documents/MySQL/SQL Data Warehouse Project/sql-data-warehouse-project-main/sql-data-warehouse-project-main/datasets/source_crm/cust_info.csv'
 INTO TABLE bronze.crm_cust_info
 FIELDS TERMINATED BY ','
@@ -27,10 +38,11 @@ SET
     cst_gndr           = NULLIF(@cst_gndr, ''),
     cst_create_date    = NULLIF(@cst_create_date, '');
 
--- -----------------------------------------------------------------------------------------------------------------
--- -----------------------------------------------------------------------------------------------------------------
 
 
+-- -----------------------------------------------------------------------------------------------------------------
+-- CRM Folder: crm_prd_info Table
+-- -----------------------------------------------------------------------------------------------------------------
 LOAD DATA LOCAL INFILE 'C:/Users/samee/OneDrive/Documents/MySQL/SQL Data Warehouse Project/sql-data-warehouse-project-main/sql-data-warehouse-project-main/datasets/source_crm/prd_info.csv'
 INTO TABLE bronze.crm_prd_info
 FIELDS TERMINATED BY ','
@@ -54,11 +66,11 @@ SET
     prd_start_dt = NULLIF(@prd_start_dt, ''),
     prd_end_dt   = NULLIF(@prd_end_dt, '');
 
+
+
 -- -----------------------------------------------------------------------------------------------------------------
+-- CRM Folder: crm_sales_details Table
 -- -----------------------------------------------------------------------------------------------------------------
-
-
-
 LOAD DATA LOCAL INFILE 'C:/Users/samee/OneDrive/Documents/MySQL/SQL Data Warehouse Project/sql-data-warehouse-project-main/sql-data-warehouse-project-main/datasets/source_crm/sales_details.csv'
 INTO TABLE bronze.crm_sales_details
 FIELDS TERMINATED BY ','
@@ -86,13 +98,13 @@ SET
     sls_quantity = NULLIF(@sls_quantity, ''),
     sls_price    = NULLIF(@sls_price, '');
 
+
+
 -- -----------------------------------------------------------------------------------------------------------------
+-- ERP Folder: erp_cust_az12 Table
 -- -----------------------------------------------------------------------------------------------------------------
-
-
-
 LOAD DATA LOCAL INFILE 'C:/Users/samee/OneDrive/Documents/MySQL/SQL Data Warehouse Project/sql-data-warehouse-project-main/sql-data-warehouse-project-main/datasets/source_erp/CUST_AZ12.csv'
-INTO TABLE bronze.erp_CUST_AZ12
+INTO TABLE bronze.erp_cust_az12
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
@@ -106,11 +118,11 @@ SET
     bdate = NULLIF(@bdate, ''),
     gen   = NULLIF(@gen, '');
 
+
+
 -- -----------------------------------------------------------------------------------------------------------------
+-- ERP Folder: erp_loc_a101 Table
 -- -----------------------------------------------------------------------------------------------------------------
-
-
-
 LOAD DATA LOCAL INFILE 'C:/Users/samee/OneDrive/Documents/MySQL/SQL Data Warehouse Project/sql-data-warehouse-project-main/sql-data-warehouse-project-main/datasets/source_erp/LOC_A101.csv'
 INTO TABLE bronze.erp_loc_a101
 FIELDS TERMINATED BY ','
@@ -124,11 +136,11 @@ SET
     cid   = NULLIF(@cid, ''),
     cntry = NULLIF(@cntry, '');
 
+
+
 -- -----------------------------------------------------------------------------------------------------------------
+-- ERP Folder: erp_px_cat_g1v2 Table
 -- -----------------------------------------------------------------------------------------------------------------
-
-
-
 LOAD DATA LOCAL INFILE 'C:/Users/samee/OneDrive/Documents/MySQL/SQL Data Warehouse Project/sql-data-warehouse-project-main/sql-data-warehouse-project-main/datasets/source_erp/PX_CAT_G1V2.csv'
 INTO TABLE bronze.erp_px_cat_g1v2
 FIELDS TERMINATED BY ','
